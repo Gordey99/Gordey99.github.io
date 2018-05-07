77900753aaccc4eaa6c094f2157f83ac8ff7450b
Gordey99 <gordey.alekseev@inbox.ru>
2018-05-08 06:59:38 +1000

    8

--- js/custom.js
+++ js/custom.js
@@ -143,6 +143,15 @@ $('#link').click(function(){
 		   
 	};
 
+	$(document).ready(function(){
+    $("#main-head").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+	});
+
 	AcupunctureClinic.init();
 
 	// Load Event
